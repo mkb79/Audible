15e98a111fb59c0a4d260e415dc3259510d75636
mkb79 <mkb79@hackitall.de>
2020-10-15 17:46:23 +0200

    Bump version to 0.4.2

--- src/audible/_version.py
+++ src/audible/_version.py
@@ -2,7 +2,7 @@ __title__ = "audible"
 __description__ = "A(Sync) Interface for internal Audible API written in " \
                   "pure Python."
 __url__ = "https://github.com/mkb79/audible"
-__version__ = "0.4.1"
+__version__ = "0.4.2"
 __author__ = "mkb79"
 __author_email__ = "mkb79@hackitall.de"
 __license__ = "AGPL"
