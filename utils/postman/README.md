5256fd9e347df5bcf722604d214795124848b2bd
mkb79 <mkb79@hackitall.de>
2020-11-01 17:27:42 +0100

    ADD GUIDE: Use Postman to request Audible API

---
+++ utils/postman/README.md
@@ -0,0 +1,12 @@
+# Use Postman to request the Audible API #
+
+
+To authenticate to the Audible API, the request have to be signed with the adp_token and the device_private_key obtained after a device registration. [Postman](https://www.postman.com/) don't support this out-of-the-box.
+
+To use Postman for requesting the API, this [pre-request-script](https://github.com/mkb79/Audible/utils/postman/pm_pre_request.js) and the [postman-util-lib](https://joolfe.github.io/postman-util-lib/) is needed.
+
+## HOWTO ##
+
+- Install the postman-util-lib
+- Copy the content from the pre-request-script into the `Pre-request Scripts` Tab for the Collection or the single request
+- Create an Environment and define the variables `adp-token` and `private-key` with the counterparts from the auth file
---
+++ utils/postman/pm_pre_request.js
@@ -0,0 +1,35 @@
+eval( pm.globals.get('pmlib_code') );
+var CryptoJS = require("crypto-js");
+var moment = require("moment");
+
+const adpToken = pm.environment.get("adp-token")
+const privateKey = pm.environment.get("private-key").replace(/\\n/g, "\n")
+
+signRequest(pm.request, adpToken, privateKey);
+
+function signRequest(request, adpToken, privateKey) {
+    const method = request.method;
+    const path = request.url.getPathWithQuery();
+    const body = request.body || "";
+    const date = moment.utc().format();
+    const data = `${method}\n${path}\n${date}\n${body}\n${adpToken}`;
+    var sig = new pmlib.rs.KJUR.crypto.Signature({"alg": "SHA256withRSA"});
+    sig.init(privateKey);
+    var hash = sig.signString(data);
+    const signedEncoded = CryptoJS.enc.Base64.stringify(CryptoJS.enc.Hex.parse(hash));
+
+    pm.request.headers.add({
+        key: 'x-adp-token',
+        value: adpToken
+    });
+
+    pm.request.headers.add({
+        key: 'x-adp-alg',
+        value: 'SHA256withRSA:1.0'
+    });
+
+    pm.request.headers.add({
+        key: 'x-adp-signature',
+        value: `${signedEncoded}:${date}`
+    });
+}
