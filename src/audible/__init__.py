99cb4ffd8f0290e7fc04c869f1dfeff51f158b37
mkb79 <mkb79@hackitall.de>
2020-11-09 22:46:35 +0100

    BUGFIX: __init__.py

--- src/audible/__init__.py
+++ src/audible/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .client import AudibleAPI, Client, AsyncClient
+from .client import Client, AsyncClient
 from .auth import LoginAuthenticator, FileAuthenticator
 from ._logging import log_helper
 from audible._version import __version__
